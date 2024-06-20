aa4d26eb252a796ac3c81cdb42dfb89c3f3cc40a
CoolCoolTomato <2983315455@qq.com>
2024-06-21 00:59:12 +0800

    规范文件格式

--- Server/docker/image.go
+++ Server/docker/image.go
@@ -1,47 +1,47 @@
 package docker
 
 import (
-    "github.com/CoolCoolTomato/MatrilxArena/Server/model"
-    "github.com/CoolCoolTomato/MatrilxArena/Server/utils/request"
+	"github.com/CoolCoolTomato/MatrilxArena/Server/model"
+	"github.com/CoolCoolTomato/MatrilxArena/Server/utils/request"
 )
 
 func GetImageList(dockerNode model.DockerNode) (map[string]interface{}, error) {
-    url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathGetImageList
+	url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathGetImageList
 	return request.Post(url, nil)
 }
 
-func GetImage(dockerNode model.DockerNode, ImageID string) (map[string]interface{}, error) {
-    url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathGetImage
+func GetImage(dockerNode model.DockerNode, imageID string) (map[string]interface{}, error) {
+	url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathGetImage
 	data := map[string]interface{}{
-		"ImageID": ImageID,
+		"ImageID": imageID,
 	}
 	return request.Post(url, data)
 }
 
-func PullImage(dockerNode model.DockerNode, RepoTags string, Repository string) (map[string]interface{}, error) {
-    url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathPullImage
+func PullImage(dockerNode model.DockerNode, repoTags string, repository string) (map[string]interface{}, error) {
+	url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathPullImage
 	data := map[string]interface{}{
-		"RepoTags": RepoTags,
-		"Repository": Repository,
+		"RepoTags":   repoTags,
+		"Repository": repository,
 	}
 	return request.Post(url, data)
 }
 
-func PullImageWithAuth(dockerNode model.DockerNode, RepoTags string, Repository string, Username string, Password string) (map[string]interface{}, error) {
-    url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathPullImageWithAuth
-    data := map[string]interface{}{
-		"RepoTags": RepoTags,
-		"Repository": Repository,
-		"Username": Username,
-		"Password": Password,
+func PullImageWithAuth(dockerNode model.DockerNode, repoTags string, repository string, username string, password string) (map[string]interface{}, error) {
+	url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathPullImageWithAuth
+	data := map[string]interface{}{
+		"RepoTags":   repoTags,
+		"Repository": repository,
+		"Username":   username,
+		"Password":   password,
 	}
 	return request.Post(url, data)
 }
 
-func RemoveImage(dockerNode model.DockerNode, ImageID string) (map[string]interface{}, error) {
-    url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathRemoveImage
+func RemoveImage(dockerNode model.DockerNode, imageID string) (map[string]interface{}, error) {
+	url := "http://" + dockerNode.Host + ":" + dockerNode.Port + PathRemoveImage
 	data := map[string]interface{}{
-		"ImageID": ImageID,
+		"ImageID": imageID,
 	}
 	return request.Post(url, data)
 }
